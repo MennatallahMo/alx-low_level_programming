f234c29decb931fa60d33821920ed9f23192df73
Mennatallah Mohamed <menna.hesham2003@gmail.com>
2023-04-21 16:37:34 -0700

    p

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - Entry Point
- * 
- * Description : A c program to run grater than and less than numbers using if and if else 
+ * main : A c program using if and if else.
+ *
+ * Return : Always 0 (Success) 
  */
 int main(void)
 {
 	int n;
-
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	}
-	else if (n==0)
+	else if (n == 0)
 	{
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n",n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
