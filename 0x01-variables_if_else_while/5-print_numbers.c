cb4c2c0ab763f33745cd38a27c458b336d4764f3
Mennatallah Mohamed <menna.hesham2003@gmail.com>
2023-04-23 11:45:57 -0700

    digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description : Program that prints all digits.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%i", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
