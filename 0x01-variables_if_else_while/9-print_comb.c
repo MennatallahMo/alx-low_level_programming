f03448b7c795717f41a3c26887c2f06f46b4d62b
Mennatallah Mohamed <menna.hesham2003@gmail.com>
2023-04-24 05:53:29 -0700

    comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description : print sinle digit numbers.
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n + 48);
+
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
