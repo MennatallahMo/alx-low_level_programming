4ed8c4b9e16f2f527e7e6b69eea368ddd19696bc
Mennatallah Mohamed <menna.hesham2003@gmail.com>
2023-05-04 12:15:36 -0700

    dest

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,9 @@
 #include "main.h"
 
 /**
+ * _strcat - function that concatenates two strings.
+ *
+ *
  * @dest: pointer to destination input
  * @src: pointer to source input
  * Return: pointer to resulting string @dest
