bc1c996430221f5936c5c3ee3384df13ffc13a64
Mennatallah Mohamed <menna.hesham2003@gmail.com>
2023-05-17 02:48:03 -0700

    memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * free_grid - a function that frees a 2 dimensional grid
+ * @grid : memory grid
+ * @height:integer
+ * Return: NULL
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
