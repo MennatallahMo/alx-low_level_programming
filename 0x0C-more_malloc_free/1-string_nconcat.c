a10c211fdf139ccacb79a9bc304deae02d9316cd
Mennatallah Mohamed <menna.hesham2003@gmail.com>
2023-05-19 02:13:20 -0700

    noncat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int i, j, s1_length, s2_length;
+	unsigned int i = 0, j = 0, s1_length = 0, s2_length = 0;
 
 	if (s1 == NULL)
 		s1 = "";
