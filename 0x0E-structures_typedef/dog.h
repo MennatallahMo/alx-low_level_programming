9f5f9a576a8e1e941aa107b35b4cc41d293fc808
Mennatallah Mohamed <menna.hesham2003@gmail.com>
2023-05-22 05:05:00 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ * Dscription: a dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
