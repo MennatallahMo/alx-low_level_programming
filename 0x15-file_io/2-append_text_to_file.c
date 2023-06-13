8e5148e335373472427352729ec4a4f9ae727217
Mennatallah Mohamed <menna.hesham2003@gmail.com>
2023-06-13 08:07:10 -0700

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,3 +16,27 @@ int _strlen(char *s)
 		i++;
 	return (i);
 }
+
+/**
+ * append_text_to_file - a function that appends text at the end of a file.
+ * @filename: file name
+ * @text_content: text
+ * Return: 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t byte = 0, len = _strlen(text_content);
+	int i;
+
+	if (!filename)
+		return (-1);
+
+	i = open(filename, O_WRONLY | O_APPEND);
+
+	if (i == -1)
+		return (-1);
+	if (len)
+		byte = write(i, text_content, len);
+	close(i);
+	return (byte == len ? 1 : -1);
+}
