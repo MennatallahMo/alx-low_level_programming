6ce144ff295eab1e8dd46dca99ec9354b2c5bdb3
Mennatallah Mohamed <menna.hesham2003@gmail.com>
2023-08-25 15:23:25 -0700

    dynamic

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+
+
+
+/**
+ * @a - first integer
+ * @b - second integer
+ * add - adds two integers
+ * sub - subtracts two integers
+ * mul - multiplies integers
+ * div - divides 2 integers
+ * mod - finds modulus
+ * Return -  sum , mul , div ,sub of a and b
+ */
+
+a = random.randint(-111, 111)
+b = random.randint(-111, 111)
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int sub(int a, int b)
+{
+	return a - b;
+}
+
+int mul(int a, int b)
+{
+	return a * b;
+}
+
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");
+		return 0;
+	}
+	return a / b;
+}
+
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");
+		retun 0;
+	}
+	return a % b;
+}
