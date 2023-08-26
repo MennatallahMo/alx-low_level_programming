59c51c5fc838d0abdbe60bdf16fa1133d5b94474
Mennatallah Mohamed <menna.hesham2003@gmail.com>
2023-08-26 06:10:05 -0700

    calc

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,7 +1,4 @@
-///#include <stdio.h>
-
-
-
+#include <stdio.h>
 
 /**
  * @a - first integer
@@ -15,37 +12,37 @@
  */
 
 
-//int add(int a, int b)
-//{
-//	return a + b;
-//}
+int add(int a, int b)
+{
+	return a + b;
+}
 
-//int sub(int a, int b)
-//{
-//	return a - b;
-//}
+int sub(int a, int b)
+{
+	return a - b;
+}
 
-//int mul(int a, int b)
-//{
-//	return a * b;
-//}
+int mul(int a, int b)
+{
+	return a * b;
+}
 
-//int div(int a, int b)
-//{
-///	if (b == 0)
-//	{
-//		printf("Error: Division by zero\n");
-//		return 0;
-//	}
-//	return a / b;
-//}
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");
+		return 0;
+	}
+	return a / b;
+}
 
-//int mod(int a, int b)
-//{
-//	if (b == 0)
-//	{
-//		printf("Error: Division by zero\n");
-//		retun 0;
-//	}
-//	return a % b;
-//}
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");
+		return 0;
+	}
+	return a % b;
+}
